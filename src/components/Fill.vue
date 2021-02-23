e4812007cbf5036cbf34a4b99c38becdeca95271
xavambro <xavambro@gmail.com>
2021-02-23 15:34:52 +0100

    Exercici1

---
+++ src/components/Fill.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+    
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Fill',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Pare.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+import Fill from './Fill'
+export default {
+  name: 'Pare',
+  props: {
+    msg: String
+  },
+  components:{
+      Fill
+  }
+}
+</script>
+
+<style>
+</style>
