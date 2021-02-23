cf07b8ab192ddf73e81052ee8c26a46336864a92
xavambro <xavambro@gmail.com>
2021-02-23 16:45:31 +0100

    Exercici 6

--- src/components/Fill.vue
+++ src/components/Fill.vue
@@ -1,17 +1,28 @@
 <template>
-<div>
-    
-</div>
+    <div class="item-component my-4 mx-auto p-2">
+       <h3>{{name}}</h3> 
+       <button class="btn btn-success" @click="counter++">{{counter}}</button>
+    </div>
 </template>
 
 <script>
+
 export default {
   name: 'Fill',
-  props: {
-    msg: String
-  }
+  data(){
+      return {
+          counter : 0
+      }
+  },
+  props:['name']
 }
 </script>
 
-<style>
+<style scoped>
+    .item-component{
+        width:30%;
+        background-color:white;
+        border:solid green; 
+
+    }
 </style>
--- src/components/Pare.vue
+++ src/components/Pare.vue
@@ -1,20 +1,30 @@
 <template>
-    <div>
+    <div class="container">
+        <h3 class="mt-3">Component Pare</h3>
+        <Fill v-for="item in items" v-bind:key="item.id" :name="item"></Fill>
     </div>
 </template>
 
 <script>
 import Fill from './Fill'
+
 export default {
   name: 'Pare',
-  props: {
-    msg: String
-  },
   components:{
       Fill
+  },
+  data(){
+     return{
+         items: ["item 1","item 2","item 3"]
+     } 
   }
 }
 </script>
 
-<style>
+<style scoped>
+    .container{
+        background-color:rgb(175, 170, 170);
+        border:solid green 3px;
+        border-radius:2rem;
+    }
 </style>
